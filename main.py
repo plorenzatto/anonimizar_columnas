bf64f9a4db0f6ecd8020ed3b235f3632aea43fb5
PabloL <enko0341@gmail.com>
2017-09-21 01:11:43 -0300

    First

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
-from helpers.helpers import *
+
+import sys
+
+
+from helpers import load_config, is_a_valid_conf, load_input, anonymize_cols, write_csv
+
 import argparse
 
-parser = argparse.ArgumentParser(description='Script para anonimizar clumnas en un CSV.')
-parser.add_argument('-c', '--config',
-                    help='Configuracion de columnas',
-                    required=False,
-                    default='config.sample.json')
-parser.add_argument('-i', '--input',
-                    help='CSV que se desea anonimizar',
-                    required=False,
-                    default='data.sample.csv')
-parser.add_argument('-o', '--output',
-                    help='Nombre del Archivo de salida',
-                    required=False,
-                    default='output.csv')
-
-input_file = parser.parse_args().__dict__.get('input')
-config = parser.parse_args().__dict__.get('config')
-output_fn = parser.parse_args().__dict__.get('output')
-
 if __name__ == '__main__':
-    conf = load_config(config_path=config)
-    if conf and is_a_valid_conf(_conf=conf):
-        # Load resource
-        status = write_csv(anonymize_cols(load_input(_input_filename=input_file), conf['columns']), output_fn)
-        if status:
-            print ('Se genero correctamente \"{}\".'.format(status))
-        else:
-            print ('Ocurrio un fallo a procesar el archivo: \"{}\".'.format(input_file))
 
-    else:
+    parser = argparse.ArgumentParser(description='Script para anonimizar clumnas en un CSV.')
+    parser.add_argument('-c', '--config' help='Configuracion de columnas', required=True)
+    parser.add_argument('-i', '--input', help='CSV que se desea anonimizar', required=True)
+    parser.add_argument('-o', '--output', help='Nombre del Archivo de salida', required=True)
+
+    args = parser.parse_args()
+
+    conf = load_config(args.conf)
+    if not is_a_valid_conf(args.conf):
+        sys.exit(1)
         print ('Configuracion invalida, no es posible continuar.')
+
+    # Load resource
+    input_data = load_input(args.input)
+    anonymized_data = anonymize_cols(input_data, conf['columns'])
+    status = write_csv(anonymized_data, args.output)
+
+    if status:
+        print ('Se genero correctamente "%s".' % status)
+    else:
+        print ('Ocurrio un fallo a procesar el archivo: "%s".' % args.input)
